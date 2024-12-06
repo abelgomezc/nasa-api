87877e06c659d572aa424b3c606b05c6eecc43c2
abelgomezc <abelgomezcondo@gmail.com>
2024-12-06 17:41:20 -0500

    agregar animaciones con tranciciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ApodComponent } from './components/nasa/apod/apod.component';
 import { EpicComponent } from './components/nasa/epic/epic.component';
 import { MarsRoverComponent } from './components/nasa/mars-rover/mars-rover.component';
 import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
    {path:'home', component:HomeComponent},
@@ -13,4 +14,10 @@ export const routes: Routes = [
     { path: '**', redirectTo: '/home' } // Ruta para páginas no encontradas
 
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })], // Habilitar modo hash
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
 
